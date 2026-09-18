f5e5bb5466eda5b4fed14c0e31bf80d3dbf56cd9
hssin123 <hamoudahssin212@gmail.com>
2026-09-18 14:28:31 +0100

    first

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM eclipse-temurin:17-jdk
-
-COPY target/formation-1.0.jar formation-1.0.jar
-
-EXPOSE 8282
-
-ENTRYPOINT ["java", "-jar", "/formation-1.0.jar"]
--- src/main/java/tn/formation/controllers/ProduitRestController.java
+++ src/main/java/tn/formation/controllers/ProduitRestController.java
@@ -18,7 +18,7 @@ public class ProduitRestController {
 
     // http://localhost:8089/SpringMVC/produit/retrieve-all-produits
     @GetMapping("/retrieve-all-produits")
-    @ResponseBody
+
     public List<Produit> getProduits() {
         List<Produit> list = produitService.retrieveAllProduits();
         return list;
@@ -26,30 +26,30 @@ public class ProduitRestController {
 
     // http://localhost:8089/SpringMVC/produit/retrieve-produit/8
     @GetMapping("/retrieve-produit/{produit-id}")
-    @ResponseBody
+
     public Produit retrieveRayon(@PathVariable("produit-id") Long produitId) {
         return produitService.retrieveProduit(produitId);
     }
 
     /* Ajouter en produit tout en lui affectant la catégorie produit et le stock associés */
-    // http://localhost:8089/SpringMVC/produit/add-produit/{idCategorieProduit}/{idStock}
+
     @PostMapping("/add-produit")
-    @ResponseBody
+
     public Produit addProduit(@RequestBody Produit p) {
         Produit produit = produitService.addProduit(p);
         return produit;
     }
 
-    // http://localhost:8089/SpringMVC/produit/remove-produit/{produit-id}
+
     @DeleteMapping("/remove-produit/{produit-id}")
-    @ResponseBody
+
     public void removeProduit(@PathVariable("produit-id") Long produitId) {
         produitService.deleteProduit(produitId);
     }
 
-    // http://localhost:8089/SpringMVC/produit/modify-produit/{idCategorieProduit}/{idStock}
+
     @PutMapping("/modify-produit")
-    @ResponseBody
+
     public Produit modifyProduit(@RequestBody Produit p) {
         return produitService.updateProduit(p);
     }
